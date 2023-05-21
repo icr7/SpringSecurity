659ad5249b272a260f77ff54f77a9b68804b75ee
ishwar chaiser <ishwarchaiser@gmail.com>
2023-05-21 14:08:09 +0530

    in memory autentication

---
+++ SpringSecurity/src/main/java/com/icr7/SpringSecurity/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.icr7.SpringSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("ishu").password("ishu123").roles("admin");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
