5aa61feecc4ba2c6b917791752c6051a984911bf
ishwar chaiser <ishwarchaiser@gmail.com>
2023-05-21 14:46:10 +0530

    implemented authorization

--- SpringSecurity/src/main/java/com/icr7/SpringSecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/icr7/SpringSecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.icr7.SpringSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,7 +13,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("ishu").password("ishu123").roles("admin");
+        auth.inMemoryAuthentication().withUser("ishu").password("ishu123").roles("ADMIN")
+                .and().withUser("mohit").password("mohit123").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/controller/admin").hasRole("ADMIN")
+                .antMatchers("/controller/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/**").permitAll()
+                .and().formLogin();
     }
 
     @Bean
